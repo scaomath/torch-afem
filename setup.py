48039409bc2e15a3ecc4a29f85682f2d614962dd
Shuhao Cao <4467042+scaomath@users.noreply.github.com>
2024-04-19 12:51:19 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'torch-fem',
+  packages=find_packages(include=['torch_fem', 'torch_fem.*']),
+  version = '0.2.1',
+  license='MIT',
+  description = 'PyTorch Finite Element Method',
+  long_description='PyTorch Finite Element Method',
+  long_description_content_type="text/markdown",
+  author = 'Shuhao Cao',
+  author_email = 'scao.math@gmail.com',
+  url = 'https://github.com/scaomath/torch-fem',
+  keywords = ['pytorch', 'fem', 'pde'],
+  install_requires=[
+      'seaborn',
+      'torchinfo',
+      'numpy',
+      'torch>=1.9.0',
+      'plotly',
+      'scipy',
+      'psutil',
+      'matplotlib',
+      'tqdm',
+      'PyYAML',
+  ],
+  classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Science/Research',
+      'Topic :: Scientific/Engineering :: Mathematics',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.8',
+  ],
+)
